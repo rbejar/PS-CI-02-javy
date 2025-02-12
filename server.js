d4b46f0b6760a1f6ae964e4c1a3214c1c3eaa186
F. Javier Zarazaga-Soria <javy@unizar.es>
2025-02-12 12:57:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8082;
 server = app.listen(PORT, () => {
   console.log(`Servidor web en el puerto ${PORT}.`);
 });
